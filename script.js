843c2d51a75732b747973d77b48b0e25dfe723de
Gil Mark <gmark131@gmail.com>
2023-07-27 15:18:15 -0400

    Rework logic to be less buggy

--- script.js
+++ script.js
@@ -1,69 +1,99 @@
+function click(e) {
+    let buttonId = e.srcElement.id;
+    let buttonClass = e.srcElement.className;
+    //console.log(e)
+    
+    if (buttonClass === 'num') {
+        ('+-/*'.includes(expression[expression.length - 1])) ? screen.innerText += ' ' + buttonId : screen.innerText += buttonId;
+        expression.push(+buttonId);
+    }
+
+    if (buttonId === '.') {
+        screen.innerText += buttonId;
+        expression.push(buttonId);
+    }
+    
+    if (buttonClass === 'operator') {
+        if (expression.length === 0 || '+-/*'.includes(expression[expression.length - 1])) { return }
+
+        if (expression.length >= 3 && operatorIndex) {
+            calculate();
+        }
+
+        screen.innerText += ' ' + buttonId;
+        expression.push(buttonId);
+
+        operatorIndex = expression.length - 1;
+    }
+
+    if (buttonId === '=') {
+        calculate()
+    }
+
+    if (buttonId === 'clear') {
+        expression = [];
+        screen.innerText = '';
+        operatorIndex = undefined;
+    }
+
+    if (buttonId === 'delete') {
+        discard = expression.pop();
+        screen.innerText = screen.innerText.slice(0, screen.innerText.length - 2); 
+    }
+    
+    console.log(expression);
+}
+
+function calculate() {
+    if (!operatorIndex || '+-*/'.includes(expression[expression.length - 1])) { return }
+
+    let operator = expression[operatorIndex];
+    let a = +expression.slice(0, operatorIndex).join('');
+    let b = +expression.slice(operatorIndex + 1).join('');
+
+    switch (operator) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+    }
+
+    screen.innerText = result;
+    expression = [result];
+    operatorIndex = undefined;
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    function subtract(a, b) {
+        return a - b;
+    }
+
+    function multiply(a, b) {
+        return a * b;
+    }
+
+    function divide(a, b) {
+        return a / b;
+    }
+}
+
 let buttons = document.querySelectorAll('button');
 for (const button of buttons) {
     button.addEventListener('click', (e) => click(e));
 }
 
-let operand1 = 0;
-let operator = '';
-let operand2 = 0;
-
-function click(e) {
-    let buttonId = e.srcElement.id;
-    
-    
-    if (buttonId === 'clear') { 
-        clear()
-    }
-    
-    //if it's a digit, add it to one of the operands
-    if (/^\d+$/.test(+buttonId)) {
-        addToScreen(buttonId);
-        if (!operator) { 
-            operand1 += buttonId;
-        } else {
-            operand2 += buttonId;
-        }
-    }
-
-    //if it's an operator, set the operator and add to screen
-    if ('+-*/'.includes(buttonId)) {
-        addToScreen(` ${buttonId} `);
-        operator = buttonId;
-    }
-
-    //if =, calculate
-    if (buttonId === '=') {
-        if (!operator) { return }
-        
-        let result = calculate(+operand1, operator, +operand2);
-        clear()
-        addToScreen(result);
-        operand1 = result;
-    }
-}
-
-function addToScreen(input) {
-    let screen = document.querySelector('#screen span');
-    screen.innerText += input;
-}
-
-function clear() {
-    let screen = document.querySelector('#screen span');
-    screen.innerText = ''
-    operand1 = 0
-    operator = ''
-    operand2 = 0
-}
-
-function calculate(operand1, operator, operand2) {
-    switch (operator) {
-        case '+':
-            return operand1 + operand2;
-        case '-':
-            return operand1 - operand2;
-        case '*':
-            return operand1 * operand2;
-        case '/':
-            return operand1 / operand2;
-    }
-}
+let screen = document.querySelector('#screen span');
+let expression = [];
+let operatorIndex;
+let result;
+let discard;
